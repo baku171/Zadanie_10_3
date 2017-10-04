7a85cbb6b6890d4d1e1fb519ecc5884544274a73
baku171 <pbak@esri.pl>
2017-10-04 09:19:00 +0200

    Zadanie 11.4

--- js/script.js
+++ js/script.js
@@ -15,3 +15,19 @@ $(function(){
         carouselList.css({marginLeft: 0});
     }
 });
+
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".")
+}
+
+var SamsungGalaxyS6 = new Phone("Samsung", 2000, "black");
+var iPhone6s = new Phone("Apple", 3000, "silver");
+var OnePlusOne = new Phone("One", 1000, "red");
+
+SamsungGalaxyS6.printInfo();
