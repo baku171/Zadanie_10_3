ed9b9f79db62fb2b3e5125c36374cbac1018ecc0
baku171 <pbak@esri.pl>
2017-10-04 13:17:31 +0200

    Add dicsount

--- js/script.js
+++ js/script.js
@@ -25,9 +25,12 @@ function Phone(brand, price, color) {
 Phone.prototype.printInfo = function() {
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 };
+Phone.prototype.applyDiscount = function(discount) {
+    this.price = this.price - discount;
+}
 
-var SamsungGalaxyS6 = new Phone("Samsung", 2000, "black");
+var samsungGalaxyS6 = new Phone("Samsung", 2000, "black");
 var iPhone6s = new Phone("Apple", 3000, "silver");
-var OnePlusOne = new Phone("One", 1000, "red");
+var onePlusOne = new Phone("One", 1000, "red");
 
-SamsungGalaxyS6.printInfo();
+samsungGalaxyS6.printInfo();
